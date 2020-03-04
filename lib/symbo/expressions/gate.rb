5bc63af2e09fff99ef2411e36232d74cabcb3d29
Yasuhito Takamiya <yasuhito@gmail.com>
2020-03-04 13:20:52 +0900

    feat: x ゲートを追加

--- lib/symbo/expressions/gate.rb
+++ lib/symbo/expressions/gate.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'symbo/expressions/matrix'
+
 module Symbo
   class Gate
     include Symbo
---
+++ lib/symbo/expressions/x_gate.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'symbo/expressions/gate'
+
+module Symbo
+  class XGate < Gate
+    def matrix
+      Matrix[[0, 1], [1, 0]]
+    end
+  end
+end
