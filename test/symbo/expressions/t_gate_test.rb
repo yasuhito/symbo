2be2962e73eacb9cd5f5b61fbcb7dfc2363f5ecc
Yasuhito Takamiya <yasuhito@gmail.com>
2020-03-05 14:57:35 +0900

    refactor: t ゲートのテストをテンソル積で書き換え

--- test/symbo/expressions/t_gate_test.rb
+++ test/symbo/expressions/t_gate_test.rb
@@ -2,16 +2,22 @@
 
 require 'test_helper'
 
+require 'symbo'
+
 module Symbo
   class TGateTest < ActiveSupport::TestCase
     using Symbo
 
     test 'T|0> = |0>' do
-      assert_equal Qubit['0'], TGate.new.apply(Qubit['0'], 0)
+      assert_equal Qubit['0'], T * Qubit['0']
     end
 
     test 'T|1> = e^{iπ/4}|1>' do
-      assert_equal E**(1i * PI/4) * Qubit['1'], TGate.new.apply(Qubit['1'], 0)
+      assert_equal E**(1i * PI/4) * Qubit['1'], T * Qubit['1']
+    end
+
+    test 'T1|01> = e^{iπ/4}|01>' do
+      assert_equal E**(1i * PI/4) * Qubit['01'], TensorProduct[I, T] * Qubit['01']
     end
   end
 end
