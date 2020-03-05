77190cffb513ea96c85d53da0a0d1a49770bc471
Yasuhito Takamiya <yasuhito@gmail.com>
2020-03-05 15:06:37 +0900

    refactor: rx ゲートのテストをテンソル積で書き換え

--- test/symbo/expressions/rx_gate_test.rb
+++ test/symbo/expressions/rx_gate_test.rb
@@ -2,24 +2,31 @@
 
 require 'test_helper'
 
+require 'symbo'
+
 module Symbo
   class RxGateTest < ActiveSupport::TestCase
     using Symbo
+    include Symbo
 
     test 'Rx(θ)|0> = cos(θ/2)|0> - isin(θ/2)|1>' do
-      assert_equal Cos[:θ/2] * Qubit['0'] - (1i * Sin[:θ/2]) * Qubit['1'], RxGate.new(:θ).apply(Qubit['0'], 0)
+      assert_equal Cos[:θ/2] * Qubit['0'] - (1i * Sin[:θ/2]) * Qubit['1'], Rx(:θ) * Qubit['0']
     end
 
     test 'Rx(θ)|1> = cos(θ/2)|1> - isin(θ/2)|0>' do
-      assert_equal Cos[:θ/2] * Qubit['1'] - (1i * Sin[:θ/2]) * Qubit['0'], RxGate.new(:θ).apply(Qubit['1'], 0)
+      assert_equal Cos[:θ/2] * Qubit['1'] - (1i * Sin[:θ/2]) * Qubit['0'], Rx(:θ) * Qubit['1']
     end
 
     test 'Rx(2π)|0> = cos(π)|0> - isin(π)|1> = -|0>' do
-      assert_equal(-Qubit['0'], RxGate.new(2 * :π).apply(Qubit['0'], 0))
+      assert_equal(-Qubit['0'], Rx(2 * :π) * Qubit['0'])
     end
 
     test 'Rx(2π)|1> = cos(π)|1> - isin(π)|0> = -|1>' do
-      assert_equal(-Qubit['1'], RxGate.new(2 * :π).apply(Qubit['1'], 0))
+      assert_equal(-Qubit['1'], Rx(2 * :π) * Qubit['1'])
+    end
+
+    test 'Rx(2π)1|00> = -|00>' do
+      assert_equal(-Qubit['00'], TensorProduct[I, Rx(2 * PI)] * Qubit['00'])
     end
   end
 end
